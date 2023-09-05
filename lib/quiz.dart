7366b5a4f0492334d2eff8b43df0ce953ce9ddd6
יצחק אמרגי <Isaac@h-MBP-sl-yzhq.lan>
2023-09-05 23:23:00 +0300

    step 1

---
+++ lib/quiz.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'start_screen.dart';
+import 'questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Quiz> {
+  Widget? activeScreen;
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.purple],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
